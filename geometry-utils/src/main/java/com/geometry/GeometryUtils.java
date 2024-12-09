f46efceb5055eaf225829ac034fbd1c3f1cec13e
zhur4k <kostya.zhurovich12052@gmail.com>
2024-12-09 19:33:22 +0300

    feat:change version and add new functions.

--- geometry-utils/src/main/java/com/geometry/GeometryUtils.java
+++ geometry-utils/src/main/java/com/geometry/GeometryUtils.java
@@ -1,11 +1,11 @@
 package com.geometry;
 
-
-import com.geometry.Shape;
-
 public class GeometryUtils {
 
     public static Integer getCommonSquare(Shape shape1, Shape shape2) {
         return shape1.getSquare() + shape2.getSquare();
+    }
+    public static Integer getCommonPerimeter(Shape shape1, Shape shape2) {
+        return shape1.getPerimeter() + shape2.getPerimeter();
     }
 }
